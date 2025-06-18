e073018a40a0d1e6d0b4f8dce96b23ab0f5f7206
ally2211 <amendieta.va@gmail.com>
2025-06-18 14:05:04 -0500

    create DatabaseProvider

---
+++ components/DatabaseProvider.tsx
@@ -0,0 +1,50 @@
+import * as FileSystem from 'expo-file-system';
+import { Asset } from 'expo-asset';
+import { Suspense, use } from 'react';
+import * as SQLite from 'expo-sqlite';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native/Libraries/Components/View/View';
+
+
+async function loadDatabase() {
+    const name = 'activities.db';
+    const dbPath = `${FileSystem.documentDirectory}SQLite/${name}`;
+    const fileinfo = await FileSystem.getInfoAsync(dbPath);
+
+    if (!fileinfo.exists) {
+        // If the database file does not exist, copy it from the assets folder
+        const dbAsset = require('@/assets/*' + name)
+        const dbUri = Asset.fromModule(dbAsset).uri;
+        await FileSystem.makeDirectoryAsync('${FileSystem.documentDirectory}SQLite}',
+            { intermediates: true }
+        );
+    
+        await FileSystem.downloadAsync(dbUri, dbPath);
+    
+    }
+}
+
+function useDB(){
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        loadDatabase().then (() => setLoaded(true))
+}, [])
+
+return { loaded };
+}
+
+function DatabaseProvider({ children }: { children: React.ReactNode }) {
+    const { loaded } = useDB();
+
+    if (!loaded) {
+        return null; // or a loading spinner
+    }
+
+    return (
+        <Suspense fallback=<View></View>>
+        <SQLite.SQLiteProvider useSuspense databaseName = 'activities.db'> {children} </SQLite.SQLiteProvider>
+        </Suspense>
+    );
+}
+
+export default DatabaseProvider;
